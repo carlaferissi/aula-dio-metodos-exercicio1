b9c2ce59820bcfcae80f7f2bf6e7f1266c59ca15
carlaferissi <carlaferissi@gmail.com>
2022-05-10 13:51:09 -0300

    first commit exercicio 1

---
+++ src/dio/basecamp/Main.java
@@ -0,0 +1,27 @@
+package dio.basecamp;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //calculadora
+        System.out.println("Exercicio Calculadora");
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //mensagem
+        System.out.println("Exercicio Mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem( 1);
+
+        //empréstimo
+        System.out.println("Exercicio Emprestimo");
+        Emprestimo.calcular( 1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular( 1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular( 1000, 5);
+
+    }
+}
